e59ab6f136af78d09ab3f8f80d51da3475af635a
Foad Ebrahimi <fwad40901@gmail.com>
2024-02-05 21:37:05 +0330

    Add Tailwind

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import "./index.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello Foad</h1>
+      <h1 className="text-purple-800 text-4xl font-bold italic">Hello Foad</h1>
     </div>
   );
 }
