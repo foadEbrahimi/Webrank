e108461112b9942137f4d66e6a905638a742360d
Foad Ebrahimi <fwad40901@gmail.com>
2024-02-21 18:35:38 +0330

    Fix Slider in About

--- src/Components/About/Slider/Slider.jsx
+++ src/Components/About/Slider/Slider.jsx
@@ -3,73 +3,120 @@ import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css/sea-green";
 export default function Slider() {
   return (
-    <div>
-      <Splide
-        className="items-center"
-        aria-label="My Favorite Images"
-        options={{
-          loop: true,
-          perPage: 5,
-          focus: "center",
-          autoplay: "pause",
-          direction: "rtl",
-          breakpoints: {
-            500: {
-              perPage: 2,
-            },
-            700: {
-              perPage: 3,
-            },
-            800: {
-              perPage: 4,
-            },
-            1200: {
-              perPage: 6,
-            },
-            1300: {
-              perPage: 7,
-            },
+    <Splide
+      className="items-center"
+      aria-label="My Favorite Images"
+      options={{
+        loop: true,
+        perPage: 5,
+        focus: "center",
+        autoplay: true,
+        direction: "rtl",
+        arrows: false,
+        breakpoints: {
+          500: {
+            perPage: 2,
           },
-          paginationDirection: "rtl",
-        }}
-      >
-        <SplideSlide>
-          <img src="./Images/Slider/img1.png" className="w-32 " alt="" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src="./Images/Slider/img2.png" className="w-32 " alt="Image 2" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src="./Images/Slider/img3.png" className="w-32 " alt="" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src="./Images/Slider/img4.png" className="w-32 " alt="Image 2" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src="./Images/Slider/img5.png" className="w-32 " alt="Image 2" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src="./Images/Slider/img1.png" className="w-32 " alt="" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src="./Images/Slider/img2.png" className="w-32 " alt="Image 2" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src="./Images/Slider/img3.png" className="w-32 " alt="" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src="./Images/Slider/img4.png" className="w-32 " alt="Image 2" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src="./Images/Slider/img5.png" className="w-32 " alt="Image 2" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src="./Images/Slider/img5.png" className="w-32 " alt="Image 2" />
-        </SplideSlide>
-        <SplideSlide>
-          <img src="./Images/Slider/img5.png" className="w-32 " alt="Image 2" />
-        </SplideSlide>
-      </Splide>
-    </div>
+          700: {
+            perPage: 3,
+          },
+          800: {
+            perPage: 4,
+          },
+          1200: {
+            perPage: 6,
+          },
+          1300: {
+            perPage: 7,
+          },
+        },
+        paginationDirection: "rtl",
+      }}
+    >
+      <SplideSlide>
+        <img
+          src="./Images/Slider/img1.png"
+          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
+          alt=""
+        />
+      </SplideSlide>
+      <SplideSlide>
+        <img
+          src="./Images/Slider/img2.png"
+          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
+          alt="Image 2"
+        />
+      </SplideSlide>
+      <SplideSlide>
+        <img
+          src="./Images/Slider/img3.png"
+          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
+          alt=""
+        />
+      </SplideSlide>
+      <SplideSlide>
+        <img
+          src="./Images/Slider/img4.png"
+          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
+          alt="Image 2"
+        />
+      </SplideSlide>
+      <SplideSlide>
+        <img
+          src="./Images/Slider/img5.png"
+          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
+          alt="Image 2"
+        />
+      </SplideSlide>
+      <SplideSlide>
+        <img
+          src="./Images/Slider/img1.png"
+          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
+          alt=""
+        />
+      </SplideSlide>
+      <SplideSlide>
+        <img
+          src="./Images/Slider/img2.png"
+          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
+          alt="Image 2"
+        />
+      </SplideSlide>
+      <SplideSlide>
+        <img
+          src="./Images/Slider/img3.png"
+          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
+          alt=""
+        />
+      </SplideSlide>
+      <SplideSlide>
+        <img
+          src="./Images/Slider/img4.png"
+          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
+          alt="Image 2"
+        />
+      </SplideSlide>
+      <SplideSlide>
+        <img
+          src="./Images/Slider/img5.png"
+          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
+          alt="Image 2"
+        />
+      </SplideSlide>
+      <SplideSlide>
+        <img
+          src="./Images/Slider/img5.png"
+          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
+          alt="Image 2"
+        />
+      </SplideSlide>
+      <SplideSlide>
+        <img
+          src="./Images/Slider/img5.png"
+          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
+          alt="Image 2"
+        />
+      </SplideSlide>
+    </Splide>
   );
 }
